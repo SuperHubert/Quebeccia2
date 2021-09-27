62cc9ecc7c1956bebf4b9dacb513a614e00d08b3
SuperHubert <80095018+SuperHubert@users.noreply.github.com>
2021-09-27 15:20:49 +0200

    player control

--- Quebeccia2/Assets/Scripts/PlayerController.cs
+++ Quebeccia2/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public GameObject playerObject;
+    public Transform playerObject;
     public float maxHeight = 4.75f;
+    public float minHeight = -4.75f;
+    public bool up;
+    public bool down;
+    public bool right;
+    public bool left;
+
+    [Range(0,100)]
+    public float speed = 1;
     
     void Start()
     {
@@ -16,10 +24,32 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //check inputs
+        up = Input.GetKey(KeyCode.Z);
+        down = Input.GetKey(KeyCode.S);
     }
 
     void FixedUpdate()
     {
         //bouge le vaisseau
+        if (up)
+        {
+            if (playerObject.transform.position.y + speed * Time.deltaTime < maxHeight) 
+            {
+                playerObject.transform.position =
+                    new Vector3(playerObject.transform.position.x,
+                        playerObject.transform.position.y + speed * Time.deltaTime,
+                        playerObject.transform.position.z);
+            }   
+        }
+        if (down)
+        {
+            if (playerObject.transform.position.y + speed * Time.deltaTime > minHeight)
+            {
+                playerObject.transform.position =
+                    new Vector3(playerObject.transform.position.x,
+                        playerObject.transform.position.y - speed * Time.deltaTime,
+                        playerObject.transform.position.z);
+            }
+        }
     }
 }
