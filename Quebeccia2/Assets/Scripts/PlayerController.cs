e94b738e4cfe246d011788e88291c0110594931e
SuperHubert <80095018+SuperHubert@users.noreply.github.com>
2021-09-27 11:54:20 +0200

    added textmeshpro

---
+++ Quebeccia2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject playerObject;
+    public float maxHeight = 4.75f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //check inputs
+    }
+
+    void FixedUpdate()
+    {
+        //bouge le vaisseau
+    }
+}
