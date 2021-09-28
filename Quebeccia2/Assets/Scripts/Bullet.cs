e5347c2ff7b10549b68d9c1d2286c91509fd4dc4
SuperHubert <80095018+SuperHubert@users.noreply.github.com>
2021-09-28 17:09:42 +0200

    Life System Update feat. collision

--- Quebeccia2/Assets/Scripts/Bullet.cs
+++ Quebeccia2/Assets/Scripts/Bullet.cs
@@ -6,43 +6,22 @@ using UnityEngine.UI;
 
 public class Bullet : MonoBehaviour
 {
-    public Image lifeBarP1;
-    public Image lifeBarP2;
-
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Enemy enemy = other.GetComponent<Enemy>();
         
-        if (enemy != null)
-        {
-            enemy.currentHp--;
-            if (enemy.currentHp <= 0)
-            {
-                Destroy(enemy.gameObject);
-                gameObject.SetActive(false);
-            }
-        }
-        else
-        {
-            PlayerHp playerHp = other.GetComponent<PlayerHp>();
-            if (playerHp != null)
-            {
-                playerHp.currentHp--;
-                if (other.GetComponent<PlayerShoot>().isPlayer1)
-                {
-                    lifeBarP1.fillAmount = playerHp.currentHp / playerHp.startingHp;
-                }
-                else
-                {
-                    lifeBarP2.fillAmount = playerHp.currentHp / playerHp.startingHp;
-                }
-                if (playerHp.currentHp <= 0)
-                {
-                    Debug.Log("Game Over"); 
-                }
-            }
-        }
+        Enemy enemy = other.GetComponent<Enemy>();
+        PlayerHp playerHp = other.GetComponent<PlayerHp>();
 
-
+        if (playerHp != null)
+        {
+            playerHp.TakeDamage(1);
+        }
+        else if (enemy != null)
+        {
+            enemy.TakeDamage(1);
+        }
+        
+        gameObject.SetActive(false);
     }
 }
--- Quebeccia2/Assets/Scripts/PlayerHp.cs
+++ Quebeccia2/Assets/Scripts/PlayerHp.cs
@@ -1,15 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHp : MonoBehaviour
 {
     public int startingHp = 6;
     public int currentHp;
     public bool canTakeDamage = true;
+
+    public Image lifeBar;
     
+    public Rigidbody2D rb;
+
     void Start()
     {
         currentHp = startingHp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (canTakeDamage)
+        {
+            currentHp -= damage;
+            if (currentHp <= 0)
+            {
+                currentHp = 0;
+                Debug.Log("Game Over"); 
+            }
+            lifeBar.fillAmount = (float)currentHp / startingHp;
+        }
+        
     }
 }
