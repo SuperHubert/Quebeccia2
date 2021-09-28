55c745423af17a19de20d06d3f0ebd98f2625bb8
Noxxeur <80094950+Noxxeur@users.noreply.github.com>
2021-09-28 16:09:30 +0200

    tentative de pv

--- Quebeccia2/Assets/Scripts/Bullet.cs
+++ Quebeccia2/Assets/Scripts/Bullet.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bullet : MonoBehaviour
 {
+    public Image lifeBarP1;
+    public Image lifeBarP2;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -25,6 +28,14 @@ public class Bullet : MonoBehaviour
             if (playerHp != null)
             {
                 playerHp.currentHp--;
+                if (other.GetComponent<PlayerShoot>().isPlayer1)
+                {
+                    lifeBarP1.fillAmount = playerHp.currentHp / playerHp.startingHp;
+                }
+                else
+                {
+                    lifeBarP2.fillAmount = playerHp.currentHp / playerHp.startingHp;
+                }
                 if (playerHp.currentHp <= 0)
                 {
                     Debug.Log("Game Over"); 
