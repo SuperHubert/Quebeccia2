13f115c25248bef84236712494357b8e8a90a5cf
SuperHubert <80095018+SuperHubert@users.noreply.github.com>
2021-09-30 21:11:48 +0200

    timing activation

--- Quebeccia2/Assets/Scripts/Enemies/Enemy.cs
+++ Quebeccia2/Assets/Scripts/Enemies/Enemy.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,11 +10,16 @@ public class Enemy : MonoBehaviour
     public int currentHp;
 
     public int scoreIncrease = 15;
+    [Tooltip("Une seule décimale après la virgule s.v.p")]
     public float timeUntilActive = 1;
+
+    public bool active = false;
 
     void Start()
     {
         currentHp = maxHp;
+        StartCoroutine(StartMoving(timeUntilActive+1));
+
     }
     
     private void OnCollisionEnter2D(Collision2D other)
@@ -38,6 +44,29 @@ public class Enemy : MonoBehaviour
             ScoreManager.Instance.AddScore(player,scoreIncrease);
             Destroy(gameObject);
             
+        }
+    }
+
+    public void Activate()
+    {
+        active = true;
+    }
+    
+    IEnumerator StartMoving(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        if (gameObject.GetComponent<MoveForward>())
+        {
+            gameObject.GetComponent<MoveForward>().canMove = true;
+        }
+        else if (gameObject.GetComponent<BackAndForth>())
+        {
+            gameObject.GetComponent<BackAndForth>().canMove = true;
+        }
+                    
+        if (gameObject.GetComponent<Shooter>())
+        {
+            gameObject.GetComponent<Shooter>().canShoot = true;
         }
     }
 }
--- Quebeccia2/Assets/Scripts/Formations/MixteV2.cs
+++ Quebeccia2/Assets/Scripts/Formations/MixteV2.cs
@@ -7,7 +7,7 @@ public class MixteV2 : MonoBehaviour
     public GameObject mainParent;
     public float mainSpeed = 1;
     public float timeSpent = 0;
-    private bool canActivate = false;
+    private bool canActivate = true;
     
     void Start()
     {
@@ -17,55 +17,12 @@ public class MixteV2 : MonoBehaviour
     
     void Update()
     {
-        if (mainParent.transform.position == Vector3.zero)
-        {
-            if (canActivate)
-            {
-                canActivate = false;
-                StartCoroutine(WaitNextPhase());
-            }
-            
-        }
-        else
+        if (mainParent.transform.position != Vector3.zero)
         {
             float step =  mainSpeed * Time.deltaTime;
             mainParent.transform.position = Vector3.MoveTowards(mainParent.transform.position, Vector3.zero, step);
         }
     }
-
-    void ActivateUnits(float unitOrder)
-    {
-        foreach (Transform branch in mainParent.transform)
-        {
-            foreach (Transform enemy in branch)
-            {
-                if (enemy.GetComponent<Enemy>().timeUntilActive == unitOrder)
-                {
-                    if (enemy.GetComponent<MoveForward>())
-                    {
-                        enemy.GetComponent<MoveForward>().canMove = true;
-                    }
-                    else if (enemy.GetComponent<BackAndForth>())
-                    {
-                        enemy.GetComponent<BackAndForth>().canMove = true;
-                    }
-                    
-                    if (enemy.GetComponent<Shooter>())
-                    {
-                        enemy.GetComponent<Shooter>().canShoot = true;
-                    }
-                }
-                
-            }
-        }
-    }
-
-    IEnumerator WaitNextPhase()
-    {
-        yield return new WaitForSeconds(0.1f);
-        timeSpent += 0.1f;
-        ActivateUnits(timeSpent);
-        canActivate = true;
-
-    }
 }
+
+
