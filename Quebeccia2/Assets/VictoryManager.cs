7a75c44841512caa8256ab7f50561dd68efa6039
Noxxeur <80094950+Noxxeur@users.noreply.github.com>
2021-10-01 11:22:21 +0200

    Victoire et Key2

--- Quebeccia2/Assets/VictoryManager.cs
+++ Quebeccia2/Assets/VictoryManager.cs
@@ -9,11 +9,38 @@ public class VictoryManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI highScoreText;
     public TextMeshProUGUI playerText;
+    public TextMeshProUGUI newHighScore;
 
     public void Menu()
     {
         SceneManager.LoadScene(0);
         Debug.Log("Menu");
+    }
+
+    private void Start()
+    {
+       if (ScoreToNextScene.Instance.scoreP1 == ScoreToNextScene.Instance.scoreP2)
+        {
+            playerText.text = "Aucun";
+            scoreText.text = "Score : " + ScoreToNextScene.Instance.scoreP1;
+        }
+       else if (ScoreToNextScene.Instance.scoreP1 > ScoreToNextScene.Instance.scoreP2)
+       {
+            playerText.text = "Joueur 1";
+            scoreText.text = "Score : " + ScoreToNextScene.Instance.scoreP1;
+        }
+       else
+        {
+            playerText.text = "Joueur 2";
+            scoreText.text = "Score : " + ScoreToNextScene.Instance.scoreP2;
+        }
+
+        highScoreText.text = "Highscore : " + ScoreToNextScene.Instance.highScore;
+        if (ScoreToNextScene.Instance.newHighScore)
+        {
+            newHighScore.text = "New !";
+        }
+
     }
 
 }
