903bf982391a024877043273d783c43ab387c642
SuperHubert <80095018+SuperHubert@users.noreply.github.com>
2021-10-01 11:19:56 +0200

    savepoint

--- Quebeccia2/Assets/Scripts/ScoreToNextScene.cs
+++ Quebeccia2/Assets/Scripts/ScoreToNextScene.cs
@@ -18,16 +18,53 @@ public class ScoreToNextScene : MonoBehaviour
     public int scoreP2;
     public int highScore;
     public bool newHighScore = false;
-    
+
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
+        if (!PlayerPrefs.HasKey("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore",0);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("HighScore", highScore);
+        }
     }
 
     public void UpdateScore()
     {
         scoreP1 = ScoreManager.Instance.player1Score;
         scoreP2 = ScoreManager.Instance.player2Score;
+        
+        if (scoreP1 > scoreP2 && scoreP1 > highScore)
+        {
+            highScore = scoreP1;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            newHighScore = true;
+        }
+        if (scoreP2 > scoreP1 && scoreP2 > highScore)
+        {
+            highScore = scoreP1;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            newHighScore = true;
+        }
+    }
 
+    
+    public string GetWinner()
+    {
+        if (scoreP1 > scoreP2)
+        {
+            return "Joueur 1";
+        }
+        else if (scoreP1 == scoreP2)
+        {
+            return "Aucun";
+        }
+        else
+        {
+            return "Joueur 2";
+        }
     }
 }
